b5aff68e0585648daa42c0f0996c93c4f7ca6715
Simon Peel <simon.peel@gmail.com>
2021-09-13 14:44:41 +0100

    Added PythonM a util for keeeping my idiot computer awake.

---
+++ mousey/PythonM.py
@@ -0,0 +1,13 @@
+import win32api, win32con
+from time import sleep
+
+while (True):  # run forever
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -1, 0, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 1, 0, 0, 0)
+    sleep(3)
+
+# import pyautogui#, sys
+# while (True):  # run forever
+#     pyautogui.moveRel(1, 1)     # move the mouse
+#     pyautogui.moveRel(-1, -1 )    # move it back!
+#     sleep(3)  # wait then start jiggling again
